d14fdb79cd70580ae3d53ee2cf5f0ad8a4def6bd
DADA2816 <anniecheng828@gmail.com>
2022-02-22 21:09:15 +0800

    no message

--- js/all.js
+++ js/all.js
@@ -9,21 +9,4 @@ $(document).ready(function () {
           scrollTop:0
         },550);
     })
-
-    $('.self-intro').on('click',function(){//繫結點選事件
-        let selfintro = $('#self-intro').offset().top;//獲取對應元素劇情螢幕頂部的距離
-        $('html,body').animate({scrollTop:selfintro},666);//動畫出爐
-    });
-    $('.script').on('click',function(){//繫結點選事件
-        let script = $('#script').offset().top;//獲取對應元素劇情螢幕頂部的距離
-        $('html,body').animate({scrollTop:script},666);//動畫出爐
-    });
-    $('.videop').on('click',function(){//繫結點選事件
-        let videop = $('#videop').offset().top;//獲取對應元素劇情螢幕頂部的距離
-        $('html,body').animate({scrollTop:videop},666);//動畫出爐
-    });
-    $('.experience').on('click',function(){//繫結點選事件
-        let experience = $('#experience').offset().top;//獲取對應元素劇情螢幕頂部的距離
-        $('html,body').animate({scrollTop:experience},666);//動畫出爐
-    });
 });
